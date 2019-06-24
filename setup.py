73259666bc9a89d35b6904bbe078761bb6dd4001
degreane <degreane@gmail.com>
2019-06-24 19:03:25 +0300

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(
+    author="Faysal Banna",
+    description="PPot is PsyLabs Honey Pot ",
+    author_email="degreane@gmail.com",
+    keywords="honeypot python3",
+    url="https://github.com/Degreane/ppot",
+    name="ppot",
+    version="0.1",
+    packages=["ppot"]
+)
